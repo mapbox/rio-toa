684b5d34fb081c47fcbe6375bf88d64f4370f138
virginiayung <virginia@mapbox.com>
2016-06-28 11:26:52 -0700

    pulling in changes from sunangle branch

---
+++ tests/test_cli.py
@@ -0,0 +1,49 @@
+import os
+
+from click.testing import CliRunner
+import logging
+import pytest
+import rasterio
+import json
+
+from rasterio.rio.options import creation_options
+from rio_toa.scripts.cli import radiance, reflectance, parsemtl
+
+
+def test_cli_radiance(tmpdir):
+    output = str(tmpdir.join('toa_radiance.tif'))
+    runner = CliRunner()
+    result = runner.invoke(radiance, 
+        ['tests/data/tiny_LC80100202015018LGN00_B1.TIF',
+         'tests/data/LC80100202015018LGN00_MTL.json',
+         output])
+    assert result.exit_code == 0
+    assert os.path.exists(output)
+
+
+def test_cli_reflectance(tmpdir):
+    output = str(tmpdir.join('toa_reflectance.tif'))
+    runner = CliRunner()
+    result = runner.invoke(reflectance, 
+        ['tests/data/tiny_LC81390452014295LGN00_B5.TIF',
+         'tests/data/LC81390452014295LGN00_MTL.json',
+         output])
+    assert result.exit_code == 0
+    with rasterio.open(output) as out:
+        assert out.count == 1
+        assert out.dtypes[0] == rasterio.float32
+
+
+def test_cli_parsemtl(tmpdir):
+    runner = CliRunner()
+    result = runner.invoke(parsemtl,
+        ['tests/data/mtltest_LC80100202015018LGN00_MTL.txt'])
+    assert result.exit_code == 0
+    assert json.loads(result.output) == dict({"L1_METADATA_FILE": {"METADATA_FILE_INFO":
+                    {"ORIGIN": "Image courtesy of the U.S. Geological Survey",
+                    "LANDSAT_SCENE_ID": "LC80100202015018LGN00",
+                    "PROCESSING_SOFTWARE_VERSION": "LPGS_2.4.0",
+                    "REQUEST_ID": "0501501184561_00001"},
+                    "PRODUCT_METADATA": {"SCENE_CENTER_TIME":
+                    "15:10:22.4142571Z", "DATE_ACQUIRED": "2015-01-18",
+                    "DATA_TYPE": "L1T"}}})
--- tests/test_sun_utils.py
+++ tests/test_sun_utils.py
@@ -1,6 +1,25 @@
 from rio_toa import sun_utils, toa_utils
 from rasterio.coords import BoundingBox
+import datetime
 import numpy as np
+
+from rio_toa.sun_utils import (
+    parse_utc_string, time_to_dec_hour, calculate_declination
+    solar_angle, sun_elevation)
+
+def test_parse_utc_string():
+    assert parse_utc_string('2014-10-22','04:37:48.7052949Z') == \
+    datetime.datetime(2014, 10, 22, 4, 37, 48)
+
+def test_time_to_dec_hour():
+    assert time_to_dec_hour(
+            datetime.datetime(2014,10, 22, 4, 37, 48)) == \
+            4.630000000000001
+
+# def test_declination():
+#     d = 10
+#     lat = 21.666821827007748
+#     assert calculate_declination(d, lat) == 
 
 def test_sun_angle():
     mtl = toa_utils._load_mtl('tests/data/LC81060712016134LGN00_MTL.json')

38b0e7077588a393d542ee8fa6e00ba3dfab54a9
dnomadb <damon@mapbox.com>
2016-07-01 14:58:25 -0700

    redo of sun elevation tools + tests

--- tests/test_sun_utils.py
+++ tests/test_sun_utils.py
@@ -1,3 +1,5 @@
+import pytest, json
+
 from rio_toa import sun_utils, toa_utils
 from rasterio.coords import BoundingBox
 import numpy as np
@@ -17,3 +19,19 @@ def test_sun_angle():
 
     assert sunangles.max() > mtl_sun
     assert sunangles.min() < mtl_sun
+
+@pytest.fixture
+def sun_elev_test_data():
+    with open('tests/data/path164sundata.json') as dsrc:
+        return json.loads(dsrc.read())
+
+def test_sun_elev_calc(sun_elev_test_data):
+    for d in sun_elev_test_data:
+        pred_sun_el = sun_utils.sun_elevation(
+            BoundingBox(*d['bbox']),
+            (10, 10),
+            d['date_acquired'],
+            d['scene_center_time']
+            )
+        assert pred_sun_el.max() > d['mtl_sun_elevation']
+        assert pred_sun_el.min() < d['mtl_sun_elevation']
